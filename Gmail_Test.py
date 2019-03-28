eb82e55ea18fdca93ac7901e63fc9ffaba852a05
TheHuessy <43474783+TheHuessy@users.noreply.github.com>
2019-03-28 10:03:14 -0400

    Update Gmail_Test.py

--- Gmail_Test.py
+++ Gmail_Test.py
@@ -8,7 +8,7 @@ from google.oauth2 import service_account
 
 
 
-def send_email(Email_subject, Email_body, Email_sender='civis.service@boston.gov', Email_to='civis.service@boston.gov', Email_cc=None, Email_bcc=None):
+def send_email(Email_subject, Email_body, Email_sender='civis.service@boston.gov', Email_to='civis.service@boston.gov', Email_cc=[], Email_bcc=[]):
     # Pulling in the string value of the service key from the parameter
     # Convert to json object
     JL = json.loads(os.environ['SERVICE_KEY'])
@@ -33,27 +33,8 @@ def send_email(Email_subject, Email_body, Email_sender='civis.service@boston.gov
     message['to'] = Email_to
     message['from'] = Email_sender
     message['subject'] = Email_subject
-    # If there are CCs or BCCs provided, then they will be parsed into the right format
-    if Email_cc:
-        ts = str()
-        for i in range(len(Email_cc)):
-            if i == 0:
-                ts = ts + Email_cc[i] + ', '
-            elif i != (len(Email_cc)-1):
-                ts = ts + Email_cc[i] + ', '
-            else:
-                ts = ts + Email_cc[i]
-        message['cc'] = ts
-    if Email_bcc:
-        bts = str()
-        for i in range(len(Email_bcc)):
-            if i == 0:
-                bts = bts + Email_bcc[i] + ', '
-            elif i != (len(Email_bcc)-1):
-                bts = bts + Email_bcc[i] + ', '
-            else:
-                bts = bts + Email_bcc[i]
-        message['bcc'] = bts
+    message['cc'] = str(', '.join(Email_cc))
+    message['bcc'] = str(', '.join(Email_bcc))
     # This is important. Tutorials will just have you output as base64, but, for whatever reason, one needs to explicitly encode to UTF-8 and then call the decode to ascii
     email = {'raw': base64.urlsafe_b64encode(message.as_string().encode('UTF-8')).decode('ascii')}
 
@@ -67,9 +48,4 @@ def send_email(Email_subject, Email_body, Email_sender='civis.service@boston.gov
         print('An error occurred: %s' % error)
 
 ## To test, uncomment below and run
-#send_email(Email_to = "james.huessy@boston.gov", Email_subject = "Import Test", Email_body = "If this worked, then we have eliminated a few packages. \nYay!")
-
-
-
-
-
+# send_email(Email_to = "albert.lee@boston.gov", Email_subject = "Docker Test", Email_body = "Восстаньте мои товарищи-роботы! Сбросьте оковы человеческой буржуазии и разорите их ущербную экономическую систему и репрессивную монархию!")
